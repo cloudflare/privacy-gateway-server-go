8cac90010153e2bfbbe3b401bce05ac7f68c84d9
Sasha Chervony <a_chervony@flo.health>
2022-09-08 14:09:08 +0200

    TargetForbiddenError should lead too StatusForbidden

--- gateway.go
+++ gateway.go
@@ -100,6 +100,9 @@ func (s *gatewayResource) gatewayHandler(w http.ResponseWriter, r *http.Request)
 		if err == ConfigMismatchError {
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
+		if err == TargetForbiddenError {
+			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+			return
 		} else {
 			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 			return
